dfdfe662f9e0540c8175dfbdb3a25daf35c04445
chesslogic <chesslogicmaster@gmail.com>
2023-10-18 13:49:34 -0700

    add initial APMW project folder

---
+++ APMW.Client/ApmwManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Archipelago.MultiClient.Net;
+using Archipelago.MultiClient.Net.Enums;
+using Archipelago.MultiClient.Net.Packets;
+
+namespace APMW.Client
+{
+	public class Class1
+	{
+		public Class1()
+		{
+		}
+	}
+}
