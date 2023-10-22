92925d675d3508680dc5a87d5faf75a1bd00f4d6
chesslogic <chesslogicmaster@gmail.com>
2023-10-21 22:48:09 -0700

    an alternative which breaks stability

--- ChessV.Base/ApmwEvents.cs
+++ ChessV.Base/ApmwEvents.cs
@@ -6,27 +6,27 @@ using System.Collections.Generic;
 namespace ChessV.Base
 {
   public class Starter
-	{
-		public static Starter _instance;
+  {
+    public static Starter _instance;
 
-		public static Starter getInstance()
-		{
-			if (_instance == null)
-			{
-				lock (typeof(Starter))
+    public static Starter getInstance()
+    {
+      if (_instance == null)
+      {
+        lock (typeof(Starter))
         {
           if (_instance == null)
-					{
-						_instance = new Starter();
-					}
+          {
+            _instance = new Starter();
+          }
         }
-			}
-			return _instance;
+      }
+      return _instance;
     }
 
     public int pocketSeed = -1;
 
-    /** Possibly not stable - will generate a different pocket distribution as the player progresses through different foundPockets - but it is uniform */
+    /** Possibly not stable - will generate a different pocket distribution as the player progresses through different foundPockets - but it is close to uniform */
     public (int, int, int) generatePocketValues(int foundPockets)
     {
       if (foundPockets == 0) { return (0, 0, 0); }
@@ -38,11 +38,30 @@ namespace ChessV.Base
       {
         return (x, 0, 0);
       }
-      var y = random.Next(Math.Max(0, foundPockets - 4 - x), Math.Min(foundPockets, 5));
+      var y = random.Next(Math.Max(0, foundPockets - 8), Math.Min(foundPockets, 5));
+      if (x < y)
+      {
+        (x, y) = (y, x);
+      }
       var z = foundPockets - (y + x);
       if (z < 0)
       {
         (x, y, z) = (4 - x, 4 - y, -z);
+      }
+      while (z > 4)
+      {
+        if (y == 4)
+        {
+          (x, y, z) = (x + 2, y, z - 2);
+        }
+        else if (x == 4)
+        {
+          (x, y, z) = (x, y + 2, z - 2);
+        }
+        else
+        {
+          (x, y, z) = (x + 1, y + 1, z - 2);
+        }
       }
 
       return (x, y, z);
