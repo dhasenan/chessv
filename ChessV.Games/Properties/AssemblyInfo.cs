3619ff70243c299e9117ab4bad9565c5959a1372
chesslogic <chesslogicmaster@gmail.com>
2023-10-27 20:59:01 -0700

    major: update from .NET Framework 4.5.1 to .NET Core 7.0

--- ChessV.Games/Properties/AssemblyInfo.cs
+++ ChessV.Games/Properties/AssemblyInfo.cs
@@ -1,16 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle( "ChessV.Games" )]
-[assembly: AssemblyDescription( "" )]
-[assembly: AssemblyConfiguration( "" )]
-[assembly: AssemblyCompany( "Greg Strong" )]
-[assembly: AssemblyProduct( "ChessV 2.2" )]
-[assembly: AssemblyCopyright( "COPYRIGHT (C) GREG STRONG 2012-2020" )]
 [assembly: AssemblyTrademark( "" )]
 [assembly: AssemblyCulture( "" )]
 
@@ -21,16 +11,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid( "7c3b68e6-6933-4fc4-a344-c3e21061c981" )]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion( "2.2.0.1" )]
-[assembly: AssemblyFileVersion( "2.2.0.1" )]
